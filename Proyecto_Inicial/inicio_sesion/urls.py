e32368f547bfcd18e929cc2f7e55cfdf736a717f
Joaquinalocs <joaquinalocruzsaavedra83@gmail.com>
2025-10-29 13:26:00 -0300

    prueba

--- Proyecto_Inicial/inicio_sesion/urls.py
+++ Proyecto_Inicial/inicio_sesion/urls.py
@@ -6,9 +6,9 @@ app_name = 'inicio_sesion'
 
 urlpatterns = [
     path('registro/', views.registro, name='registro'),
-    path('login/', views.login_usuario, name='login'),
+    path('', views.login_usuario, name='login'),
     path('logout/', views.logout_usuario, name='logout_usuario'),
-    path('', views.inicio, name='inicio'),
+    
     path("publicar/", views.publicar_objeto, name="publicar_objeto"),
     path("objetos/", views.listar_objetos, name="listar_objetos"),
     path("editar/<int:pk>/", views.editar_objeto, name="editar_objeto"),
